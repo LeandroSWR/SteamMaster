dda8c3ed0b107cb0448928d2fba7e726ec685c23
Leandro Bras <shadowgaming.7@gmail.com>
2021-11-07 23:20:14 +0000

    Change Dlls, Change run dependencies

--- SteamMaster/Program.cs
+++ SteamMaster/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,9 +16,37 @@ namespace SteamMaster
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SMmain());
+            bool ranFromSteamPath = Application.StartupPath.Contains(
+                (string)Registry.GetValue(
+                    @"HKEY_LOCAL_MACHINE\Software\Valve\Steam", 
+                    "InstallPath", 
+                    null));
+
+            if (ranFromSteamPath)
+            {
+                MessageBox.Show(
+                    "This tool can't be ran from within the Steam directory.,",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+
+            Steamworks.SteamAPI.Init();
+
+            if (Steamworks.SteamAPI.IsSteamRunning())
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new SMmain());
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Steam is not running. Make sure steam is running before opening this tool.",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
         }
     }
 }
